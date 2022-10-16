de2153687fb47bd6c60817dd3374b9e79c2ca33c
LcSilva0702 <lcsilva0702@gmail.com>
2022-10-16 16:54:06 -0300

    metodos

---
+++ metodos/Smart-tv/src/usuario.java
@@ -0,0 +1,10 @@
+public class usuario {
+    public static void main(String[] args) throws Exception {
+        
+        smarttv smarttv = new smarttv(); 
+        
+        System.out.println("Tv ligada:" + smarttv.ligada);
+        System.out.println("Canal atual:" + smarttv.canal);
+        System.out.println("Volume atual:" + smarttv.volume);
+    }
+}
--- tipos-e-variaveis/Tipos-variaveis/src/Tiposvariaveis.java
+++ tipos-e-variaveis/Tipos-variaveis/src/Tiposvariaveis.java
@@ -2,7 +2,14 @@ public class Tiposvariaveis{
 
  public static void main(String[] args) {
     
-    System.out.println("ola");   
+   //int = numero inteiros (mais utilizado) 
+   //double = numeros reais (mais utilizado)
+   //byte = numero inteiros de -128 a 127 
+   //long = numero inteiro 
+   //boolean = True ou false
+   //char = aceita um caracter apenas
+   //float = numeros reais menores
+   // 
  }
 
 }

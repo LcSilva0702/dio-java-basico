2cd7adbc3f6a748e38cb776e7964c1e0be5b351b
LcSilva0702 <lcsilva0702@gmail.com>
2022-10-17 08:39:36 -0300

    testando operadores

---
+++ metodos/operadores relacionais/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int i1,i2;
+        float f1,f2;
+        char c1,c2;
+        String s1,s2;
+        boolean b1,b2;
+
+        i1 = 1;
+        i2 = 2;
+
+        boolean teste = i1 == i2;
+
+        boolean teste2 = b1 > b2;
+    }
+}

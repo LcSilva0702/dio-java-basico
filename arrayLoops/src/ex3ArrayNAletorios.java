a2d5a3e8e38099753c88f2ed0a5ce4182b817393
LcSilva0702 <lcsilva0702@gmail.com>
2022-10-23 17:12:54 -0300

    Corrigindo erros

---
+++ arrayLoops/src/ex3ArrayNAletorios.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class ex3ArrayNAletorios {
+    public static void main(String[] args){
+/*
+Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+Ao final mostre os números e seus sucessores.
+*/
+        Random random = new Random();
+        
+        int[] numerosAleatorios = new int[20];
+        
+        for(int i = 0 ; i < numerosAleatorios.length ; i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+        System.out.print("\nNumeros aletorios: ");
+        
+        for(int numero : numerosAleatorios){
+            System.out.print(numero + " ");
+        }
+        System.out.print("\nSucessores: ");
+        for(int numero : numerosAleatorios){
+            System.out.print((numero + 1)+ " ");
+        }
+        System.out.print("\nAntecessores: ");
+        for(int numero : numerosAleatorios){
+            System.out.print((numero - 1)+ " ");
+        }
+    }
+}    
